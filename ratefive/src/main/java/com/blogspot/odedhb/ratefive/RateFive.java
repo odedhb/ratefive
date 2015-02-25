f15f7ed59a9ddecb6adedb6404dc528272e32435
odedhb <odedhb@gmail.com>
2015-02-25 23:20:04 +0200

    Feature: don't bug me

--- ratefive/src/main/java/com/blogspot/odedhb/ratefive/RateFive.java
+++ ratefive/src/main/java/com/blogspot/odedhb/ratefive/RateFive.java
@@ -2,8 +2,11 @@ package com.blogspot.odedhb.ratefive;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
@@ -16,11 +19,57 @@ public class RateFive {
     private final Dialog dialog;
     private RatingBar ratingBar;
     private Button rateButton;
+    private Context context;
+    private String feedbackEmail;
+    private String LAST_CHECKED = "LAST_CHECKED";
+    private String RATED = "RATED";
 
     public RateFive(final Context context, final String feedbackEmail) {
+        this.context = context;
+        this.feedbackEmail = feedbackEmail;
         dialog = new Dialog(context);
+    }
+
+    private boolean shouldShow() {
+
+        if (mem().getBoolean(RATED, false)) {
+            return false;
+        }
+
+        long lastChecked = mem().getLong(LAST_CHECKED, 0l);
+
+        if (lastChecked == 0l) {
+            rememberShown();
+            return false;
+        }
+
+        if ((System.currentTimeMillis() - lastChecked) < (DateUtils.DAY_IN_MILLIS * 3)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void rememberShown() {
+        mem().edit().putLong(LAST_CHECKED, System.currentTimeMillis()).commit();
+    }
+
+    private SharedPreferences mem() {
+        return context.getSharedPreferences(
+                context.getPackageName() + "ratefive", Context.MODE_PRIVATE);
+    }
+
+    public void show() {
+        if (!shouldShow()) return;
+
         dialog.setContentView(R.layout.dialog);
         dialog.setTitle(context.getString(R.string.title));
+        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                rememberShown();
+            }
+        });
         rateButton = (Button) dialog.findViewById(R.id.rate_button);
         ratingBar = (RatingBar) dialog.findViewById(R.id.rating_bar);
         rateButton.setOnClickListener(new View.OnClickListener() {
@@ -39,6 +88,7 @@ public class RateFive {
                         context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
                     }
                 }
+                mem().edit().putBoolean(RATED, true).commit();
                 dialog.dismiss();
             }
         });
@@ -48,10 +98,7 @@ public class RateFive {
                 rateButton.setVisibility(View.VISIBLE);
             }
         });
-    }
 
-
-    public void show() {
         dialog.show();
     }
 
